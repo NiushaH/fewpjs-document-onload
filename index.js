f2ce5105eaee340c68f54f3b888122bb46002c24
NiushaH <35556570+NiushaH@users.noreply.github.com>
2022-10-02 15:15:18 -0600

    use HTML5 defer <script> attribute

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById('text').textContent="This is really cool!";
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//   document.getElementById('text').textContent="This is really cool!";
+// });
+
+document.getElementById('text').textContent="This is really cool!";
